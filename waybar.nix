e21567966133cac845240e5495c0cb31715ba801
Justus Hansen <zendium.hansen@gmail.com>
2025-12-20 23:14:05 +0200

    test waybar conf

--- waybar.nix
+++ waybar.nix
@@ -1,12 +1,19 @@
 { config, pkgs, ... }:
 
 {
-  programs.waybar = {
-    enable = true;
-    settings.mainBar = {
-      layer = "top";
-      modules-left  = [ "niri/workspaces" ];
-      modules-right = [ "clock" ];
-    };
-  };
+	programs.waybar = {
+		enable = true;
+		settings = {
+    		mainBar = {
+      			layer = "top";
+    			modules-left  = [ "niri/workspaces" "niri/window" ];
+    			modules-right = [ "wireplumber" "network" "cpu" "clock" ];
+			 };
+    	};
+    	style = ''
+    	      * {
+    	        font-family: "Monospace";
+    	      }
+    	'';
+	};
 }
